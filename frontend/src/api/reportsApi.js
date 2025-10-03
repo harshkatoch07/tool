8665584946f91315b3d605ea1c9f910ba683aa7e
harsh <harsh.katoch@gera.in>
2025-10-04 00:42:29 +0530

    reports

--- frontend/src/api/reportsApi.js
+++ frontend/src/api/reportsApi.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import { http, authHeaders } from "./http";
 
 export async function getUserActivityReport({ username, fromUtc, toUtc }) {
   const params = new URLSearchParams({ username, fromUtc, toUtc });
-  const { data } = await axios.get(`/reports/user-activity?${params.toString()}`);
+  const { data } = await http.get(`/reports/user-activity?${params.toString()}`, {
+    headers: authHeaders(),
+  });
   return data;
 }
